0d267755a9cfa02c56ec43be10b01838168ae0fb
xypage <xypage@gmail.com>
2022-05-02 11:41:44 -0700

    now dynamically creating table using sample json

--- static/js/map.js
+++ static/js/map.js
@@ -1,11 +1,62 @@
 // We'd fill cols/rows from the database
 let rows, cols, container;
 
-function setup(_rows = 5, _cols = 5) {
-  rows = _rows;
-  cols = _cols;
+function setup(jsonInput) {
+  rows = 5;
+  cols = 5;
+  jsonInput.sort(function(a, b) {
+    if(a.Geometry.BoundingBox.Top == b.Geometry.BoundingBox.Top) {
+      return a.Geometry.BoundingBox.Left - b.Geometry.BoundingBox.Left;
+    } else {
+      return a.Geometry.BoundingBox.Top - b.Geometry.BoundingBox.Top;
+    }
+  });
   container = document.getElementById("container");
   draw();
+  makeTable(jsonInput);
+}
+
+function makeTable(jsonInput) {
+  let table = document.querySelector(".dashboard-table");
+  let count = 1;
+  jsonInput.forEach(element => {
+    let tr = document.createElement("tr");
+    tr.classList.add("dashboard-table-row");
+
+    let countElement = document.createElement("td");
+    countElement.classList.add("dashboard-table-elem");
+    countElement.innerText = count;
+    count++;
+
+    let row = document.createElement("td");
+    row.classList.add("dashboard-table-elem");
+    row.innerText = Math.floor(count / cols); // TODO make this read the individual row lenghts instead of the total # of columns
+
+    let column = document.createElement("td");
+    column.classList.add("dashboard-table-elem");
+    column.innerText = count % cols;
+
+    let confidence = document.createElement("td");
+    confidence.classList.add("dashboard-table-elem");
+    confidence.innerText = element.Confidence.toFixed(3) + "%";
+
+
+    tr.appendChild(countElement);
+    tr.appendChild(row);
+    tr.appendChild(column);
+    tr.appendChild(confidence);
+
+    table.appendChild(tr);
+  });
+
+}
+
+function makeTree(i, j) {
+  let newTree = document.createElement("div");
+  newTree.classList.add("tree");
+  newTree.innerHTML = "(" + j + ", " + i + ")";
+  
+  return newTree;
 }
 
 function draw() {
@@ -17,11 +68,7 @@ function draw() {
       let newCol = document.createElement("div");
       newCol.classList.add("tree-wrapper");
 
-      // fill the "tree" div with whatever information we want instead of just its position
-      // probably write a constructor to do that instead of having it inline
-      let newTree = document.createElement("div");
-      newTree.classList.add("tree");
-      newTree.innerHTML = "(" + j + ", " + i + ")";
+      let newTree = makeTree(i, j);
       
       newCol.appendChild(newTree);
       newRow.appendChild(newCol);
@@ -29,9 +76,8 @@ function draw() {
     container.appendChild(newRow);
   }
 }
-/*
-document.getElementById("update").onclick = 
-*/
+
+
 function updateSize() {
   rows = document.getElementById("row-input").valueAsNumber;
   cols = document.getElementById("col-input").valueAsNumber;
@@ -39,6 +85,47 @@ function updateSize() {
 }
 
 window.onload = function() {
+  let testInput = [
+    {"Geometry": {
+        "BoundingBox": {
+          "Left": 0,
+          "Top": 0,
+          "Width": 10,
+          "Height": 8
+        }
+      }, "Confidence": 87.82499694824219
+    }, {
+      "Geometry": {
+        "BoundingBox": {
+          "Left": 10,
+          "Top": 15,
+          "Width": 5,
+          "Height": 9
+        }
+      }, "Confidence": 86.03299713134766
+    }, {
+      "Geometry": {
+        "BoundingBox": {
+          "Left": 20,
+          "Top": 3,
+          "Width": 4,
+          "Height": 16
+        }
+      }, "Confidence": 82.15799713134766
+    }, {
+      "Geometry": {
+        "BoundingBox": {
+          "Left": 2,
+          "Top": 20,
+          "Width": 10,
+          "Height": 10
+        }
+      }, "Confidence": 82.6259994506836
+    }];
+ 
+
+    
   console.log("Loaded");
-  setup();
+  console.log(testInput);
+  setup(testInput);
 }
